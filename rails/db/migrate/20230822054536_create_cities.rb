57fece24917381b4674e2c6cd55ac8f0b9407d09
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-08-22 05:58:58 +0000

    文字列の制限を削除した。

--- rails/db/migrate/20230822054536_create_cities.rb
+++ rails/db/migrate/20230822054536_create_cities.rb
@@ -1,10 +1,9 @@
 class CreateCities < ActiveRecord::Migration[7.0]
   def change
     create_table :cities do |t|
-      t.integer :id, null: false, primary_key: true
       t.references :prefecture, null: false, foreign_key: true
-      t.string :name, limit: 50, null: false
-      t.string :yomi, limit: 50, null: false
+      t.string :name, null: false
+      t.string :yomi, null: false
       t.timestamps
     end
   end
--- rails/db/migrate/20230822054727_create_prefectures.rb
+++ rails/db/migrate/20230822054727_create_prefectures.rb
@@ -1,8 +1,7 @@
 class CreatePrefectures < ActiveRecord::Migration[7.0]
   def change
     create_table :prefectures do |t|
-      t.integer :id, null: false, primary_key: true
-      t.string :name, limit: 50, null: false
+      t.string :name, null: false
       t.timestamps
     end
   end
