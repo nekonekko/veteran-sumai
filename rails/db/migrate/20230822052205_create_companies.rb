d319d353bd148cfb7e8702a136d486d3669d74d3
chocolateingress <101337367+chocolateingress@users.noreply.github.com>
2023-08-22 06:26:49 +0000

    ieul_company_idの追加

--- rails/db/migrate/20230822052205_create_companies.rb
+++ rails/db/migrate/20230822052205_create_companies.rb
@@ -2,6 +2,7 @@ class CreateCompanies < ActiveRecord::Migration[7.0]
   def change
     create_table :companies do |t|
       t.string :name, null: false
+      t.integer :ieul_company_id, null: false, unique: true
       t.timestamps
     end
   end
--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_060832) do
 
   create_table "prefectures", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "name", null: false
+    t.integer "ieul_company_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
