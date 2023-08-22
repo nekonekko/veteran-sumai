d813a56079f37890f7e228a9efd09a05e8059d22
chocolateingress <101337367+chocolateingress@users.noreply.github.com>
2023-08-22 07:16:56 +0000

    BranchesのMigrationファイルの追加

--- rails/db/migrate/20230822065442_branches.rb
+++ rails/db/migrate/20230822065442_branches.rb
@@ -1,8 +1,20 @@
 class Branches < ActiveRecord::Migration[7.0]
   def change
-    create_table :companies do |t|
+    create_table :branches do |t|
       t.string :name, null: false
-      t.integer :ieul_company_id, null: false, unique: true
+      t.references :companies, null: false, foreign_key: true
+      t.integer :ieul_branch_id, null: false
+      t.string :logo_url, null: false
+      t.string :postal_code
+      t.references :prefectures, null: false, foreign_key: true
+      t.references :cities, null: false, foreign_key: true
+      t.string :address, null: false
+      t.string :phone_number
+      t.string :fax_number
+      t.string :business_hours
+      t.string :holiday
+      t.text :catch_copy, null: false
+      t.text :introduction, null: false
       t.timestamps
     end
   end
