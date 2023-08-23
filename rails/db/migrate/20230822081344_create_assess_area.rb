96e646d36bb78c5b9ae0bdd0c7dd6a7c74f0c9f8
chocolateingress <101337367+chocolateingress@users.noreply.github.com>
2023-08-23 02:35:47 +0000

    複数形を単数系に修正

--- rails/db/migrate/20230822081344_create_assess_area.rb
+++ rails/db/migrate/20230822081344_create_assess_area.rb
@@ -1,8 +1,8 @@
 class CreateAssessArea < ActiveRecord::Migration[7.0]
   def change
     create_table :assess_areas do |t|
-      t.references :offices, null: false, foreign_key: true
-      t.references :cities, null: false, foreign_key: true
+      t.references :office, null: false, foreign_key: true
+      t.references :city, null: false, foreign_key: true
       t.timestamps
     end
   end
