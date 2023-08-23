3f0a979e7fa8907d76a374a374bf3f7149b4d7ba
chocolateingress <101337367+chocolateingress@users.noreply.github.com>
2023-08-23 04:29:05 +0000

    migrationファイルの時系列れいり

--- rails/db/migrate/20230823042502_create_assess_areas.rb
+++ rails/db/migrate/20230823042502_create_assess_areas.rb
@@ -1,4 +1,6 @@
-class CreateAssessArea < ActiveRecord::Migration[7.0]
+# frozen_string_literal: true
+
+class CreateAssessAreas < ActiveRecord::Migration[7.0]
   def change
     create_table :assess_areas do |t|
       t.references :office, null: false, foreign_key: true
