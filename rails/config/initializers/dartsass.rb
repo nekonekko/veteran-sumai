1ece77ad888ee323071a414584ebd71cf921abb1
momo <87163501+nekonekko@users.noreply.github.com>
2023-08-23 09:46:44 +0000

    :art: rubocopでフォーマット

--- rails/config/initializers/dartsass.rb
+++ rails/config/initializers/dartsass.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.config.dartsass.builds = {
-    "reviews/color_stars.scss" => "color_stars.css",
+  'reviews/color_stars.scss' => 'color_stars.css'
 }
