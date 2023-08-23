2cc7ae5c7a3b6fa512d6b3cdcf4d91a58fe32ea2
chocolateingress <101337367+chocolateingress@users.noreply.github.com>
2023-08-23 04:05:08 +0000

    reviewsテーブルの命名の修正

--- rails/db/migrate/20230823021340_create_review.rb
+++ rails/db/migrate/20230823021340_create_review.rb
@@ -3,37 +3,37 @@
 class CreateReview < ActiveRecord::Migration[7.0]
   def change
     create_table :reviews do |t|
-      t.references :office, null: false, foreign_key: true
-      t.integer :ieul_office_id, null: false, uniqueness: true
-      t.string :username, null: false
-      t.integer :gender, null: false
-      t.integer :age, null: false
-      t.references :city, index: { name: 'property_city_id' }, null: false, foreign_key: true
-      t.string :property_address, null: false
-      t.integer :property_type, null: false
-      t.integer :num_of_sales, null: false
-      t.date :considered_sale_on, null: false
-      t.date :requested_assessment_on, null: false
-      t.date :started_selling_on, null: false
-      t.date :sold_on, null: false
-      t.date :transfered_on, null: false
-      t.integer :satisfaction_with_sale_speed, null: false
-      t.integer :assessed_price, null: false
-      t.integer :sale_price, null: false
-      t.boolean :is_price_reduced, null: false, default: false
-      t.integer :price_reduction_after_how_many_month, null: true
-      t.integer :reduced_price, null: true
-      t.integer :contract_price, null: false
-      t.integer :satisfaction_with_sale_price, null: false
-      t.integer :form_of_mediation_contract, null: false
-      t.text :headline, null: false
-      t.integer :reason_for_sale, null: false
-      t.text :concerns_at_time_of_sale, null: false
-      t.text :reason_for_choosing_this_agent, null: false
-      t.integer :satisfaction_with_company_response, null: false
-      t.text :reason_for_satisfaction_with_company_response, null: false
-      t.text :advice_for_future_sellers, null: false
-      t.text :improvement_desired_from_company, null: true
+      t.references :office, null: false, foreign_key: true, comment: '企業ID'
+      t.integer :ieul_office_id, null: false, uniqueness: true, comment: 'ieul_店舗ID'
+      t.string :username, null: false, comment: '名前'
+      t.integer :gender, null: false, comment: '性別'
+      t.integer :age, null: false, comment: '年齢'
+      t.references :city, index: { name: 'property_city_id' }, null: false, foreign_key: true, comment: '市区町村'
+      t.string :property_address, null: false, comment: '住所'
+      t.integer :property_type, null: false, comment: '物件種別'
+      t.integer :sale_count, null: false, comment: '売却回数'
+      t.date :considered_sale_on, null: false, comment: '売却検討時期'
+      t.date :assessment_requested_on, null: false, comment: '売却依頼時期'
+      t.date :started_selling_on, null: false, comment: '売出時期'
+      t.date :sold_on, null: false, comment: '売却時期'
+      t.date :transfered_on, null: false, comment: '引渡時期'
+      t.integer :sale_speed_evaluation, null: false, comment: '売却スピードの満足度'
+      t.integer :assessed_price, null: false, comment: '査定価格'
+      t.integer :sale_price, null: false, comment: '売却価格'
+      t.boolean :price_reduced, null: false, default: false, comment: '値下げしたかどうか'
+      t.integer :price_reduction_how_many_month, null: true, comment: '売り出して何ヶ月に値下げしたか'
+      t.integer :reduced_price, null: true, comment: '値下げ価格'
+      t.integer :contract_price, null: false, comment: '成約価格'
+      t.integer :sale_price_evaluation, null: false, comment: '売却価格の満足度'
+      t.integer :mediation_contract_form, null: false, comment: '媒体契約の形態'
+      t.text :headline, null: false, comment: '見出し'
+      t.integer :sale_reason, null: false, comment: '売却価格'
+      t.text :sale_anxiety_reason, null: false, comment: '売却時に不安だったこと'
+      t.text :choose_agent_reaseon, null: false, comment: 'この会社に決めた理由'
+      t.integer :company_response_evaluation, null: false, comment: '不動産会社の対応満足度'
+      t.text :company_response_evaluation_reason, null: false, comment: '不動産会社の対応満足度の理由'
+      t.text :advice, null: false, comment: '今後売却する人へのアドバイス'
+      t.text :improvement_point, null: true, comment: '不動産会社に改善して欲しいこと'
       t.timestamps
     end
   end
