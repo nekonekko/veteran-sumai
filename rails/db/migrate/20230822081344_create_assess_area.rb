81cb67911dea712f027cc9407129bf85df705653
chocolateingress <101337367+chocolateingress@users.noreply.github.com>
2023-08-23 01:42:02 +0000

    branchからofficeに命名変更

--- rails/db/migrate/20230822081344_create_assess_area.rb
+++ rails/db/migrate/20230822081344_create_assess_area.rb
@@ -1,8 +1,8 @@
 class CreateAssessArea < ActiveRecord::Migration[7.0]
   def change
     create_table :assess_areas do |t|
-      t.references :branch, null: false, foreign_key: true
-      t.references :city, null: false, foreign_key: true
+      t.references :offices, null: false, foreign_key: true
+      t.references :cities, null: false, foreign_key: true
       t.timestamps
     end
   end
