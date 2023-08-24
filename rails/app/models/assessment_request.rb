7f140281236b4797c35fc9eb9ea0d34a90c885d7
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-08-24 09:25:01 +0000

    rubocopに通るようにコードを修正した。

--- rails/app/models/assessment_request.rb
+++ rails/app/models/assessment_request.rb
@@ -1,10 +1,15 @@
-class AssessmentRequest < ActiveRecord::Base
-    belongs_to :office
-    belongs_to :city
+# frozen_string_literal: true
 
-    # 空チェック
-    validates :office, :city, :property_address, :property_type, :property_exclusive_area, :property_land_area, :property_building_area, :property_building_area_unit, :property_floor_area, :url_param, :property_room_plan, :property_constructed_year, :user_email, :user_name, :user_name_kana, :user_tel, presence: true
-    # メールアドレス、電話番号のバリデーション
-    validates  :user_email , length: {maximum: 100}, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i  }
-    validates :user_tel, length: {minimum: 10, maximum: 11}, format: {with: /\A0\d+\z/}
+class AssessmentRequest < ApplicationRecord
+  belongs_to :office
+  belongs_to :city
+
+  # 空チェック
+  validates :property_address, :property_type, :property_exclusive_area, :property_land_area,
+            :property_building_area, :property_building_area_unit, :property_floor_area, :url_param,
+            :property_room_plan, :property_constructed_year, :user_email, :user_name, :user_name_kana,
+            :user_tel, presence: true
+  # メールアドレス、電話番号のバリデーション
+  validates :user_email, length: { maximum: 100 }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :user_tel, length: { minimum: 10, maximum: 11 }, format: { with: /\A0\d+\z/ }
 end
--- rails/spec/models/assessment_request_spec.rb
+++ rails/spec/models/assessment_request_spec.rb
@@ -1,134 +1,142 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe AssessmentRequest, type: :model do
-    describe '空白のバリデーションテスト' do
-        it 'すべての情報が正しく入力されている時、有効であること' do
-            ass = build(:assessment_request)
-            expect(ass).to  be_valid
-        end
-
-        it '物件の住所が空の場合、無効であること' do
-            ass = build(:assessment_request, property_address: nil)
-            expect(ass).to_not be_valid
-          end
-      
-          it '物件の種別が空の場合、無効であること' do
-            ass = build(:assessment_request, property_type: nil)
-            expect(ass).to_not be_valid
-          end
-      
-          it '物件の専有面積が空の場合、無効であること' do
-            ass = build(:assessment_request, property_exclusive_area: nil)
-            expect(ass).to_not be_valid
-          end
-      
-          it '物件の土地面積が空の場合、無効であること' do
-            ass = build(:assessment_request, property_land_area: nil)
-            expect(ass).to_not be_valid
-          end
-      
-          it '物件の建物面積が空の場合、無効であること' do
-            ass = build(:assessment_request, property_building_area: nil)
-            expect(ass).to_not be_valid
-          end
-      
-          it '物件の建物面積の単位が空の場合、無効であること' do
-            ass = build(:assessment_request, property_building_area_unit: nil)
-            expect(ass).to_not be_valid
-          end
-      
-          it '物件の階数が空の場合、無効であること' do
-            ass = build(:assessment_request, property_floor_area: nil)
-            expect(ass).to_not be_valid
-          end
-      
-          it 'URLパラメータが空の場合、無効であること' do
-            ass = build(:assessment_request, url_param: nil)
-            expect(ass).to_not be_valid
-          end
-      
-          it '部屋のプランが空の場合、無効であること' do
-            ass = build(:assessment_request, property_room_plan: nil)
-            expect(ass).to_not be_valid
-          end
-      
-          it '築年数が空の場合、無効であること' do
-            ass = build(:assessment_request, property_constructed_year: nil)
-            expect(ass).to_not be_valid
-          end
-      
-          it 'ユーザーのメールアドレスが空の場合、無効であること' do
-            ass = build(:assessment_request, user_email: nil)
-            expect(ass).to_not be_valid
-          end
-      
-          it 'ユーザーの名前が空の場合、無効であること' do
-            ass = build(:assessment_request, user_name: nil)
-            expect(ass).to_not be_valid
-          end
-      
-          it 'ユーザーの名前（カナ）が空の場合、無効であること' do
-            ass = build(:assessment_request, user_name_kana: nil)
-            expect(ass).to_not be_valid
-          end
-      
-          it 'ユーザーの電話番号が空の場合、無効であること' do
-            ass = build(:assessment_request, user_tel: nil)
-            expect(ass).to_not be_valid
-          end
-    end
-    describe 'フォーマットのバリデーションテスト' do
-        it 'メールアドレスが正しい形式の場合、有効であること' do
-            ass = build(:assessment_request, user_email: 'test@example.com')
-            expect(ass).to be_valid
-        end
-        it 'メールアドレスに@がない場合、無効であること' do
-            ass = build(:assessment_request, user_email: 'testexample.com')
-            expect(ass).to_not be_valid
-        end
-        it 'メールアドレスに.がない場合、無効であること' do
-            ass = build(:assessment_request, user_email: 'test@examplecom')
-            expect(ass).to_not be_valid
-        end
-        it '電話番号が正しい形式の場合、有効であること' do
-            ass = build(:assessment_request, user_tel: '08012345678')
-            expect(ass).to be_valid
-        end
-        it '電話番号が0から始まらない場合、無効であること' do
-            ass = build(:assessment_request, user_tel: '18012345678')
-            expect(ass).to_not be_valid
-        end
-    end
-    describe '文字長さのバリデーションテスト' do
-        it 'メールアドレスが範囲内の長さ（100文字以内）の場合、有効であること' do
-            ass = build(:assessment_request, user_email: 'test@example.com')
-            expect(ass).to be_valid
-        end
-        it 'メールアドレスが範囲外の長さ（101文字以上）の場合、無効であること' do
-            ass = build(:assessment_request, user_email: 'abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz@example.com
-                ')
-            expect(ass).to_not be_valid
-        end
-        it 'ユーザーの電話番号が10文字の場合、有効であること' do
-            ass = build(:assessment_request, user_tel: '0801234567')
-            expect(ass).to be_valid
-          end
-      
-          it 'ユーザーの電話番号が11文字の場合、有効であること' do
-            ass = build(:assessment_request, user_tel: '09012345678')
-            expect(ass).to be_valid
-          end
-      
-          it 'ユーザーの電話番号が9文字の場合、無効であること' do
-            ass = build(:assessment_request, user_tel: '070123456')
-            expect(ass).to_not be_valid
-          end
-      
-          it 'ユーザーの電話番号が12文字の場合、無効であること' do
-            ass = build(:assessment_request, user_tel: '070123456789')
-            expect(ass).to_not be_valid
-          end
-      
+RSpec.describe AssessmentRequest do
+  describe '空白のバリデーションテスト' do
+    it 'すべての情報が正しく入力されている時、有効であること' do
+      ass = build(:assessment_request)
+      expect(ass).to be_valid
     end
 
+    it '物件の住所が空の場合、無効であること' do
+      ass = build(:assessment_request, property_address: nil)
+      expect(ass).not_to be_valid
+    end
+
+    it '物件の種別が空の場合、無効であること' do
+      ass = build(:assessment_request, property_type: nil)
+      expect(ass).not_to be_valid
+    end
+
+    it '物件の専有面積が空の場合、無効であること' do
+      ass = build(:assessment_request, property_exclusive_area: nil)
+      expect(ass).not_to be_valid
+    end
+
+    it '物件の土地面積が空の場合、無効であること' do
+      ass = build(:assessment_request, property_land_area: nil)
+      expect(ass).not_to be_valid
+    end
+
+    it '物件の建物面積が空の場合、無効であること' do
+      ass = build(:assessment_request, property_building_area: nil)
+      expect(ass).not_to be_valid
+    end
+
+    it '物件の建物面積の単位が空の場合、無効であること' do
+      ass = build(:assessment_request, property_building_area_unit: nil)
+      expect(ass).not_to be_valid
+    end
+
+    it '物件の階数が空の場合、無効であること' do
+      ass = build(:assessment_request, property_floor_area: nil)
+      expect(ass).not_to be_valid
+    end
+
+    it 'URLパラメータが空の場合、無効であること' do
+      ass = build(:assessment_request, url_param: nil)
+      expect(ass).not_to be_valid
+    end
+
+    it '部屋のプランが空の場合、無効であること' do
+      ass = build(:assessment_request, property_room_plan: nil)
+      expect(ass).not_to be_valid
+    end
+
+    it '築年数が空の場合、無効であること' do
+      ass = build(:assessment_request, property_constructed_year: nil)
+      expect(ass).not_to be_valid
+    end
+
+    it 'ユーザーのメールアドレスが空の場合、無効であること' do
+      ass = build(:assessment_request, user_email: nil)
+      expect(ass).not_to be_valid
+    end
+
+    it 'ユーザーの名前が空の場合、無効であること' do
+      ass = build(:assessment_request, user_name: nil)
+      expect(ass).not_to be_valid
+    end
+
+    it 'ユーザーの名前（カナ）が空の場合、無効であること' do
+      ass = build(:assessment_request, user_name_kana: nil)
+      expect(ass).not_to be_valid
+    end
+
+    it 'ユーザーの電話番号が空の場合、無効であること' do
+      ass = build(:assessment_request, user_tel: nil)
+      expect(ass).not_to be_valid
+    end
+  end
+
+  describe 'フォーマットのバリデーションテスト' do
+    it 'メールアドレスが正しい形式の場合、有効であること' do
+      ass = build(:assessment_request, user_email: 'test@example.com')
+      expect(ass).to be_valid
+    end
+
+    it 'メールアドレスに@がない場合、無効であること' do
+      ass = build(:assessment_request, user_email: 'testexample.com')
+      expect(ass).not_to be_valid
+    end
+
+    it 'メールアドレスに.がない場合、無効であること' do
+      ass = build(:assessment_request, user_email: 'test@examplecom')
+      expect(ass).not_to be_valid
+    end
+
+    it '電話番号が正しい形式の場合、有効であること' do
+      ass = build(:assessment_request, user_tel: '08012345678')
+      expect(ass).to be_valid
+    end
+
+    it '電話番号が0から始まらない場合、無効であること' do
+      ass = build(:assessment_request, user_tel: '18012345678')
+      expect(ass).not_to be_valid
+    end
+  end
+
+  describe '文字長さのバリデーションテスト' do
+    it 'メールアドレスが範囲内の長さ（100文字以内）の場合、有効であること' do
+      ass = build(:assessment_request, user_email: 'test@example.com')
+      expect(ass).to be_valid
+    end
+
+    it 'メールアドレスが範囲外の長さ（101文字以上）の場合、無効であること' do
+      ass = build(:assessment_request, user_email: 'abcdefghijklmnopqrstuvwxyz1234567890abcdefgh
+        ijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz@example.com')
+      expect(ass).not_to be_valid
+    end
+
+    it 'ユーザーの電話番号が10文字の場合、有効であること' do
+      ass = build(:assessment_request, user_tel: '0801234567')
+      expect(ass).to be_valid
+    end
+
+    it 'ユーザーの電話番号が11文字の場合、有効であること' do
+      ass = build(:assessment_request, user_tel: '09012345678')
+      expect(ass).to be_valid
+    end
+
+    it 'ユーザーの電話番号が9文字の場合、無効であること' do
+      ass = build(:assessment_request, user_tel: '070123456')
+      expect(ass).not_to be_valid
+    end
+
+    it 'ユーザーの電話番号が12文字の場合、無効であること' do
+      ass = build(:assessment_request, user_tel: '070123456789')
+      expect(ass).not_to be_valid
+    end
+  end
 end
