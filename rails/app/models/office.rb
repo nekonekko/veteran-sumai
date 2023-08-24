127628304f5d95cdd4ecb792483b6d202bc6d006
momo <87163501+nekonekko@users.noreply.github.com>
2023-08-24 10:03:33 +0000

    :sparkles: 評価計算関数の雛形を作成

--- rails/app/models/office.rb
+++ rails/app/models/office.rb
@@ -11,4 +11,49 @@ class Office < ApplicationRecord
   validates :address, presence: true
   validates :catch_copy, presence: true
   validates :introduction, presence: true
+
+  def calculate_review_mean(office_id, include_speed=true, include_response=true, include_price=true)
+    # office_idの店舗の口コミの満足度の平均値を計算する
+    # include_speed, include_response, include_priceはそれぞれ、売却スピードの満足度、対応満足度、売却価格の満足度を
+    # 計算に含めるかどうかを指定する
+    # 3つともfalseにするとエラーが出るようにしてあります
+
+    reviews = Review.find_by_office_id(office_id)
+
+    if reviews.length == 0
+      return 0
+    end
+
+    if include_speed == false && include_response == false && include_price == false
+      raise ArgumentError, '少なくとも1つはtrueにしてください！'
+    end
+
+    sum = 0
+    reviews.each do |review|
+      if include_speed
+        sum += review.sale_speed_evaluation
+      end
+      if include_response
+        sum += review.company_response_evaluation
+      end
+      if include_price
+        sum += review.sale_price_evaluation
+      end
+    end
+
+    sum /= reviews.length
+
+    num_of_points = 0
+    if include_speed
+      num_of_points += 1
+    end
+    if include_response
+      num_of_points += 1
+    end
+    if include_price
+      num_of_points += 1
+    end
+
+    return sum / num_of_points
+  end
 end
