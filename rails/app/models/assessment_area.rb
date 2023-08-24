0a4a573ef451908ab4ae58674a4d450c2c4a98b3
chocolateingress <101337367+chocolateingress@users.noreply.github.com>
2023-08-24 07:36:39 +0000

    assesment_areaのModelファイル、テストファイルの削除

--- rails/app/models/assessment_area.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class AssessmentArea < ApplicationRecord
-  belongs_to :office
-  belongs_to :city
-end
--- rails/spec/factories/assesment_area.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :assessment_area do
-    office
-    city
-  end
-end
