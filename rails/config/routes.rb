352d9d0b1e6f58f7454e73c47e524e5f75db42a5
momo <87163501+nekonekko@users.noreply.github.com>
2023-08-24 01:59:44 +0000

    :truck: 本来のroutingをコメントアウトした状態で追加

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # get 'offices/:id', to: 'offices#show' <- controller作成後はこっちに
   get 'offices', to: 'offices#show'
 
   mount KomachiHeartbeat::Engine => '/ops'
