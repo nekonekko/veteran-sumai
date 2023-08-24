50ce6abb4f369390c58de36af8f09c0589b6cffe
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-08-24 05:11:25 +0000

    仮でrspecのファイルを作った。

---
+++ rails/spec/models/assessment_request_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe AssessmentRequest, type: :model do
+    pending "add some examples to (or delete) #{__FILE__}"
+    describe 'バリデーションテスト' do
+        it '名前がからの時、登録に失敗すること' do
+            
+        end
+    end
+end
