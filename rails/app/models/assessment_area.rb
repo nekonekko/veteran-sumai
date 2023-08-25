0712e97355da44127c69f4d4f026d7bc6d4bf599
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-08-25 07:40:39 +0000

    uniquenessのバリデーションの表記を修正した。

--- rails/app/models/assessment_area.rb
+++ rails/app/models/assessment_area.rb
@@ -4,6 +4,6 @@ class AssessmentArea < ApplicationRecord
   belongs_to :office
   belongs_to :city
 
-  validates: :office_id, uniqueness: { scope:  :office_id }
-  validates: :city_id:  uniqueness: { scope: :city_id }
+  validates :office_id, uniqueness: { scope: :city_id }
+  validates :city_id, uniqueness: { scope: :office_id }
 end
