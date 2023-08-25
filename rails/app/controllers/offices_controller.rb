5734e5a4540db40f80412862a1b0807d9588c6f1
momo <87163501+nekonekko@users.noreply.github.com>
2023-08-25 05:04:35 +0000

    :sparkles: erbに企業と店舗の変数を埋め込み

--- rails/app/controllers/offices_controller.rb
+++ rails/app/controllers/offices_controller.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class OfficesController < ApplicationController
-  def show; end
+  def show
+    # TODO: 存在しないidにアクセスした場合の処理は issue #65 で追加予定
+    # issueのリンク：https://github.com/speee/hr-eng-internship-2023-1st-team-summer-tech-circle/issues/65
+    @office = Office.find(params[:id])
+  end
 end
