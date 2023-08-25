3f31999c47130262d750c1b51322ef38426dfddc
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-08-25 07:36:55 +0000

    :office_idとcity_idにuniquenessを設定した。

--- rails/app/models/assessment_area.rb
+++ rails/app/models/assessment_area.rb
@@ -3,4 +3,7 @@
 class AssessmentArea < ApplicationRecord
   belongs_to :office
   belongs_to :city
+
+  validates: :office_id, uniqueness: { scope:  :office_id }
+  validates: :city_id:  uniqueness: { scope: :city_id }
 end
