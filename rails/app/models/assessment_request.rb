da014f2ae7263a4a539df80b81756b0c74afb52c
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-08-25 07:16:21 +0000

    メールアドレスのバリデーションをオリジナルのものに直した。

--- rails/app/models/assessment_request.rb
+++ rails/app/models/assessment_request.rb
@@ -10,6 +10,6 @@ class AssessmentRequest < ApplicationRecord
             :property_room_plan, :property_constructed_year, :user_email, :user_name, :user_name_kana,
             :user_tel, presence: true
   # メールアドレス、電話番号のバリデーション
-  validates :user_email, length: { maximum: 100 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :user_email, length: { maximum: 100 }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
   validates :user_tel, format: { with: /(\A0\d{9}\z)|(\A0\d{10}\z)/ }
 end
