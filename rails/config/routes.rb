2aa995cde0884fa7073e7874d62716eedb98b398
momo <87163501+nekonekko@users.noreply.github.com>
2023-08-25 05:03:28 +0000

    :truck: 店舗ページのURLを修正しidを追加

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # get 'offices/:id', to: 'offices#show' <- controller作成後はこっちに
-  get 'offices', to: 'offices#show', as: :office
+  get 'offices/:id', to: 'offices#show'
   get 'prefectures/:prefecture_id/cities/:city_id', to: 'cities#show', as: :city
   get 'assessment/:office_id', to: 'assessment#show'
+
   mount KomachiHeartbeat::Engine => '/ops'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
