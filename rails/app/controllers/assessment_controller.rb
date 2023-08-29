d9fa4ffd22624fd81f38fe20ef2ddd2ef4410e8a
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-08-29 06:13:20 +0000

    rubocopを通るようにコードを修正した。

--- rails/app/controllers/assessment_controller.rb
+++ rails/app/controllers/assessment_controller.rb
@@ -2,18 +2,18 @@
 
 class AssessmentController < ApplicationController
   def show
-    if params[:office_id] != nil then
+    if params[:office_id].nil?
+      @office_id = params[:office_id]
+    else
       office_id = params[:office_id]
       assessment_areas = Office.find(office_id).assessment_areas
       @available_cities = []
       assessment_areas.each do |area|
         city_id = area.city.id
         city_name = area.city.name
-        city_hash = {id: city_id, name: city_name}
+        city_hash = { id: city_id, name: city_name }
         @available_cities.push(city_hash)
       end
-    else
-      @office_id = params[:office_id]
     end
   end
 
