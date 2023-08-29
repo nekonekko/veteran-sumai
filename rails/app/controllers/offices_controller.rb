d4bc194529e0bee48ee3b3f187c0652060f1793d
momo <87163501+nekonekko@users.noreply.github.com>
2023-08-29 03:37:32 +0000

    :lipstick: チャートに変数を埋め込み

--- rails/app/controllers/offices_controller.rb
+++ rails/app/controllers/offices_controller.rb
@@ -5,5 +5,10 @@ class OfficesController < ApplicationController
     # TODO: 存在しないidにアクセスした場合の処理は issue #65 で追加予定
     # issueのリンク：https://github.com/speee/hr-eng-internship-2023-1st-team-summer-tech-circle/issues/65
     @office = Office.find(params[:id])
+    @reviews = @office.reviews
+    @speed_evaluation_mean = @office.calculate_review_mean(response: false, price: false)
+    @response_evaluation_mean = @office.calculate_review_mean(speed: false, price: false)
+    @price_evaluation_mean = @office.calculate_review_mean(speed: false, response: false)
+    @total_evaluation_mean = @office.calculate_review_mean()
   end
 end
