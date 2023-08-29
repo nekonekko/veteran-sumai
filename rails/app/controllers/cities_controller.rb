f8e9fc780fa61727ff675e46c98def29355a6ae3
momo <87163501+nekonekko@users.noreply.github.com>
2023-08-29 09:30:27 +0000

    :sparkles: 該当する店舗のリストを取得

--- rails/app/controllers/cities_controller.rb
+++ rails/app/controllers/cities_controller.rb
@@ -3,5 +3,10 @@
 class CitiesController < ApplicationController
   def show
     @city = City.find(params[:id])
+
+    @offices = []
+    AssessmentArea.where(city: @city).each do |assesment_area|
+      @offices.push(assesment_area.office)
+    end
   end
 end
