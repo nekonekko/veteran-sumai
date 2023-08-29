b773350705d7d538b3eba4f630ecdc0b3a5443f5
momo <87163501+nekonekko@users.noreply.github.com>
2023-08-29 08:26:01 +0000

    :recycle: インスタンス変数をcityのみにし、partialにはlocal変数を渡す

--- rails/app/controllers/cities_controller.rb
+++ rails/app/controllers/cities_controller.rb
@@ -3,8 +3,5 @@
 class CitiesController < ApplicationController
   def show
     @city = City.find(params[:id])
-    @prefecture = Prefecture.find(params[:prefecture_id])
-
-    @cities = @prefecture.cities
   end
 end
