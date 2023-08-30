69786e5c51ca5d7be7a9b7e8d7fc23a530833b28
chocolateingress <101337367+chocolateingress@users.noreply.github.com>
2023-08-30 08:29:27 +0000

    sslの部分をDevelopment環境のみにする

--- rails/app/models/assessment_request.rb
+++ rails/app/models/assessment_request.rb
@@ -21,7 +21,9 @@ class AssessmentRequest < ApplicationRecord
     uri = URI.parse('https://miniul-api.herokuapp.com/affiliate/v2/conversions')
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    if Rails.env.development?
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    end
 
     req = Net::HTTP::Post.new(uri.path)
     req.set_form_data(form_params)
