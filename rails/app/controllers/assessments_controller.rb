c76ff7e0abb2760927f144466d4b32e3ddc0301c
chocolateingress <101337367+chocolateingress@users.noreply.github.com>
2023-08-30 07:42:55 +0000

    査定フォームのリダイレクトテスト

--- rails/app/controllers/assessments_controller.rb
+++ rails/app/controllers/assessments_controller.rb
@@ -13,14 +13,12 @@ class AssessmentsController < ApplicationController
   end
 
   def create
-    puts **assessment_request_params
     Rails.logger.debug assessment_request_params
     assessment_request = AssessmentRequest.new(assessment_request_params)
     Rails.logger.debug params[:office_id]
     if assessment_request.valid?
         assessment_request.save!
         assessment_request.post_to_ieul!
-        puts assessment_request.success
         redirect_to assessment_request.success? ? thanks_url : error_url
     else
       flash[:alert] = assessment_request.errors.full_messages
@@ -47,45 +45,6 @@ class AssessmentsController < ApplicationController
             :property_room_plan,
             :property_constructed_year
           )
-  end
-
-  def build_form_data(params)
-    office = Office.find(params[:office_id])
-    {
-      "branch_id": Office.find(params[:office_id]).id,
-      "property_city": eval(params[:city])[:id].to_i,
-      "property_address": params[:property_address],
-      "property_type": params[:property_address].to_i,
-      "property_exclusive_area": params[:property_exclusive_area].to_f,
-      "property_land_area": params[:property_land_area].to_f,
-      "property_building_area": params[:property_building_area].to_f,
-      "url_param": 'beteran-sumai',
-      "property_room_plan": params[:room_type].to_i,
-      "property_constructed_year": params[:property_constructed_year].to_i,
-      "user_email": params[:user_email],
-      "user_name":  params[:custom_name_sei]+params[:custom_name_mei],
-      "user_name_kana": params[:custom_kana_sei]+params[:custom_kana_mei],
-      "user_tel": params[:user_tel]
-    }
-  end
-
-  def build_assessment_request(params, success)
-    AssessmentRequest.new(
-      office: Office.find(params[:office_id]),
-      city_id: eval(params[:city])[:id].to_i,
-      property_address: params[:property_address],
-      property_type: params[:property_address].to_i,
-      property_exclusive_area: params[:property_exclusive_area].to_f,
-      property_land_area: params[:property_land_area].to_f,
-      property_building_area: params[:property_building_area].to_f,
-      property_room_plan: params[:room_type].to_i,
-      property_constructed_year: params[:property_constructed_year].to_i,
-      user_email: params[:user_email],
-      user_name: params[:custom_name_sei]+params[:custom_name_mei],
-      user_name_kana: params[:custom_kana_sei]+params[:custom_kana_mei],
-      user_tel: params[:user_tel],
-      api_failure: !success
-    )
   end
 
   def thanks
