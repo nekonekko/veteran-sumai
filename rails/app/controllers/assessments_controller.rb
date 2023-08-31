2fac8e4f5defb013d210686f830906b8c7e1cb33
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-08-31 05:08:08 +0000

    デバッグ用のコードを削除した。

--- rails/app/controllers/assessments_controller.rb
+++ rails/app/controllers/assessments_controller.rb
@@ -2,7 +2,6 @@
 
 class AssessmentsController < ApplicationController
   def new
-    Rails.logger.debug params[:office_id]
     if params[:office_id].nil?
       redirect_to root_url
       return
@@ -14,20 +13,15 @@ class AssessmentsController < ApplicationController
   end
 
   def create
-    Rails.logger.debug 'a' * 100
     @assessment_request = AssessmentRequest.new(assessment_request_params)
     if @assessment_request.valid?
       @assessment_request.save!
       @assessment_request.post_to_ieul!
       redirect_to @assessment_request.success? ? thanks_url : error_url
     else
-      Rails.logger.debug 'b' * 100
-      Rails.logger.debug @assessment_request.errors.full_messages
       flash.now[:alert] = @assessment_request.errors.full_messages
       # redirect_to new_assessment_url(office_id: @assessment_request.office_id)
       @office = @assessment_request.office
-      Rails.logger.debug @office.name
-      Rails.logger.debug @office.company.name
 
       render :new
     end
